7cf7a53540ce5714c86dff292bcd57df0b2334c9
LucasHerrero <lucasherrero2010@hotmail.com>
2024-04-15 00:40:22 +0200

    Add CORS middleware to app.js

--- app.js
+++ app.js
@@ -1,9 +1,12 @@
 const express = require('express');
+const cors = require('cors');
+
 const { dbConnectMySql } = require('./source/mysql.js');
 const userController = require('./source/controller/UserController.js');
 const app = express();
 const port = 3000;
 app.use(express.json());
+app.use(cors()); // Asegúrate de que esta línea está antes de tus rutas
 
 app.use('/', userController);
 
