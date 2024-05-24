6370166254a2c0325483d17938b4d88d944d3106
LucasHerrero <lucasherrero2010@hotmail.com>
2024-05-24 13:07:03 +0200

    feat: Update IMC information messages in UserController.js
    The code changes in UserController.js update the IMC information messages in the `/imcCalculation/:userId` endpoint. The messages have been modified to use more descriptive terms for different IMC ranges. This commit aligns with the recent user commits and follows the established conventions in the repository.

--- source/controller/UserController.js
+++ source/controller/UserController.js
@@ -219,19 +219,19 @@ router.get("/imcCalculation/:userId", async (req, res) => {
         const imc = (user.kg / (user.height / 100) ** 2).toFixed(2);
         let imcInfo = "";
         if (imc < 18.5) {
-          imcInfo = "Bajo peso";
+          imcInfo = "Delgadez";
         } else if (imc >= 18.5 && imc < 24.9) {
           imcInfo = "Peso normal";
         } else if (imc >= 25 && imc < 29.9) {
           imcInfo = "Sobrepeso";
         } else if (imc >= 30 && imc < 34.9) {
-          imcInfo = "Obesidad tipo 1";
+          imcInfo = "Obesidad Moderada";
         } else if (imc >= 35 && imc < 39.9) {
-          imcInfo = "Obesidad tipo 2";
+          imcInfo = "Obesidad Severa";
         } else if (imc >= 40) {
-          imcInfo = "Obesidad tipo 3";
+          imcInfo = "Obesidad Morbida";
         }
-        console.log("Imc", imcInfo);
+
         res.json({ imc, imcInfo });
       }
     }
